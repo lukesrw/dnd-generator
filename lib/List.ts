8837dae7eb572b47e35329080b124521e727bb9e
lukesrw <lukesrw+github@gmail.com>
2021-03-31 22:07:28 +0100

    Updated retrieval from list

--- lib/List.ts
+++ lib/List.ts
@@ -1,6 +1,8 @@
-import { promises as fs } from "fs";
+// import { promises as fs } from "fs";
+import * as Generic from "../interfaces/generic";
 
 export interface ListItem {
+    [property: string]: any;
     value: string;
 }
 
@@ -10,11 +12,11 @@ export interface ListItemRaw extends ListItem {
 
 export class List {
     file: string;
-    list: ListItem[];
+    items: ListItem[];
 
     constructor(file: string) {
         this.file = file;
-        this.list = [];
+        this.items = [];
     }
 
     static pickRandom(list: any): ListItem {
@@ -25,10 +27,10 @@ export class List {
         return list;
     }
 
-    async load() {
-        let data = await fs.readFile(this.file, "utf-8");
+    getItems() {
+        let data = JSON.parse(JSON.stringify(require(this.file)));
 
-        this.list = [];
+        this.items = [];
 
         try {
             JSON.parse(data).forEach((item: ListItemRaw) => {
@@ -38,15 +40,47 @@ export class List {
 
                 item.weight = parseInt(String(item.weight), 10);
 
-                this.list = this.list.concat(new Array(item.weight).fill(item));
+                this.items = this.items.concat(
+                    new Array(item.weight).fill(item)
+                );
             });
         } catch (e) {}
 
         return this;
     }
 
-    pickRandom() {
-        let item = List.pickRandom(this.list);
+    getFiltered(filter: Generic.Object) {
+        let list = this.items;
+
+        if (filter && Object.values(filter).length > 0) {
+            filter = JSON.parse(JSON.stringify(filter));
+
+            return list.filter(item => {
+                return !Object.keys(filter).some(property => {
+                    if (!Array.isArray(filter[property])) {
+                        filter[property] = [filter[property]];
+                    }
+
+                    if (Object.prototype.hasOwnProperty.call(item, property)) {
+                        if (!Array.isArray(item[property])) {
+                            item[property] = [item[property]];
+                        }
+
+                        return item[property].every((option: any) => {
+                            return filter[property].indexOf(option) === -1;
+                        });
+                    }
+
+                    return false;
+                });
+            });
+        }
+
+        return list;
+    }
+
+    pickRandom(filter: Generic.Object) {
+        let item = List.pickRandom(this.getFiltered(filter));
 
         return item ? List.pickRandom(item.value) : false;
     }
