4dc5011b0c9a58fb5fd2df03902469f5dcf565d7
lukesrw <lukesrw+github@gmail.com>
2021-04-13 21:33:13 +0100

    Improved NPC, added getName, getDescription

--- lib/NPC.ts
+++ lib/NPC.ts
@@ -1,17 +1,24 @@
+import { Gender } from "./gender/gender";
+import { getPronoun } from "./language/common";
 import { NameList } from "./name/name";
 import { Place } from "./Place";
+import { Sex } from "./sex/sex";
+import { ucfirst } from "./utils";
 
-const RE_ROLL_GENDER_CHANCE = 50;
+const TRANSGENDER_CHANCE = 50;
+const NON_BINARY_CHANCE = 50;
 
 export class NPC {
     place: Place;
 
+    // name
     forename: string;
     surname: string;
 
-    gender: string;
-    sex: string;
+    sex: Sex;
+    gender: Gender;
 
+    age: string;
     alignment: string;
     class: string;
     motivation: string;
@@ -20,40 +27,69 @@ export class NPC {
     race: string;
     title: string;
 
+    // physical
+    hair: string;
+
     constructor(place: Place, properties?: Partial<NPC>) {
         this.place = place;
 
+        /**
+         * born as...
+         */
         if (properties && properties.sex) {
             this.sex = properties.sex;
         } else {
-            this.sex = this.place.lists.sex.pickRandom(this);
+            this.sex = this.place.lists.sex.pickRandom(
+                Object.assign({}, properties || {}, this)
+            ) as Sex;
         }
 
+        /**
+         * identify as...
+         */
         if (properties && properties.gender) {
             this.gender = properties.gender;
         } else {
             this.gender = this.sex;
-            if (Math.floor(Math.random() * RE_ROLL_GENDER_CHANCE) === 1) {
-                this.gender = this.place.lists.sex.pickRandom(this);
+            if (Math.floor(Math.random() * TRANSGENDER_CHANCE) === 1) {
+                this.gender = this.place.lists.sex.pickRandom(
+                    Object.assign({}, properties || {}, this)
+                ) as Gender;
+            } else if (Math.floor(Math.random() * NON_BINARY_CHANCE) === 1) {
+                this.gender = "Non-Binary";
             }
+        }
+
+        if (properties && properties.age) {
+            this.age = properties.age;
+        } else {
+            this.age = this.place.lists.age.pickRandom(
+                Object.assign({}, properties || {}, this)
+            );
         }
 
         if (properties && properties.race) {
             this.race = properties.race;
         } else {
-            this.race = this.place.lists.race.pickRandom(this);
+            this.race = this.place.lists.race.pickRandom(
+                Object.assign({}, properties || {}, this)
+            );
         }
 
         if (properties && properties.class) {
             this.class = properties.class;
         } else {
-            this.class = this.place.lists.class.pickRandom(this);
+            this.class = this.place.lists.class.pickRandom(
+                Object.assign({}, properties || {}, this)
+            );
         }
 
         if (properties && properties.nobility) {
             this.nobility = properties.nobility;
         } else {
-            this.nobility = this.place.lists.nobility.pickRandom(this);
+            this.nobility = this.place.lists.nobility.pickRandom(
+                Object.assign({}, properties || {}, this)
+            );
         }
 
         if (properties && properties.title) {
@@ -61,25 +97,33 @@ export class NPC {
         } else if (this.nobility === "Knighted") {
             this.title = this.gender === "Male" ? "Sir" : "Dame";
         } else {
-            this.title = this.place.lists.title.pickRandom(this);
+            this.title = this.place.lists.title.pickRandom(
+                Object.assign({}, properties || {}, this)
+            );
         }
 
         if (properties && properties.alignment) {
             this.alignment = properties.alignment;
         } else {
-            this.alignment = this.place.lists.alignment.pickRandom(this);
+            this.alignment = this.place.lists.alignment.pickRandom(
+                Object.assign({}, properties || {}, this)
+            );
         }
 
         if (properties && properties.motivation) {
             this.motivation = properties.motivation;
         } else {
-            this.motivation = this.place.lists.motivation.pickRandom(this);
+            this.motivation = this.place.lists.motivation.pickRandom(
+                Object.assign({}, properties || {}, this)
+            );
         }
 
         if (properties && properties.profession) {
             this.profession = properties.profession;
         } else {
-            this.profession = this.place.lists.profession.pickRandom(this);
+            this.profession = this.place.lists.profession.pickRandom(
+                Object.assign({}, properties || {}, this)
+            );
         }
 
         this.surname = "";
@@ -95,5 +139,29 @@ export class NPC {
             this.forename = (forename || "").trim();
             this.surname = (surname || "").trim();
         }
+
+        if (properties && properties.hair) {
+            this.hair = properties.hair;
+        } else {
+            this.hair = this.place.lists.hair.pickRandom(
+                Object.assign({}, properties || {}, this)
+            );
+        }
+    }
+
+    getName() {
+        return [this.forename, this.surname].filter(name => name).join(" ");
+    }
+
+    getDescription() {
+        return `${this.getName()} (${this.profession}): ${this.race} ${
+            this.class
+        }, ${this.alignment}. ${this.forename} is {physical detail} with ${
+            this.hair
+        } hair and {eye color} eyes. ${ucfirst(
+            getPronoun("third", this.gender, "subject")
+        )} wears {armour} and wields {weapon} (and {weapon 2}). ${
+            this.forename
+        } seeks ${this.motivation} (age: ${this.age}%).`;
     }
 }
