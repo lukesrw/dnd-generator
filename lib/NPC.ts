b78291d7c9b1dbc49ec3f7905f45b2ec14b36a0f
lukesrw <lukesrw+github@gmail.com>
2021-04-15 23:53:54 +0100

    Improved checks for blank strings

--- lib/NPC.ts
+++ lib/NPC.ts
@@ -31,7 +31,7 @@ export class NPC {
 
     // physical
     hair: string;
-    eye: string;
+    eyes: string;
 
     constructor(place?: Place, properties: Partial<NPC> = {}) {
         this.place = place instanceof Place ? place : new Place();
@@ -39,144 +39,145 @@ export class NPC {
         /**
          * born as...
          */
-        if (properties && properties.sex) {
+        if (properties && typeof properties.sex === "string") {
             this.sex = properties.sex;
         } else {
             this.sex = this.place.lists.sex.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             ) as Sex;
         }
 
         /**
          * identify as...
          */
-        if (properties && properties.gender) {
+        if (properties && typeof properties.gender === "string") {
             this.gender = properties.gender;
         } else {
             this.gender = this.sex;
             if (Math.floor(Math.random() * TRANSGENDER_CHANCE) === 1) {
                 this.gender = this.place.lists.sex.pickRandom(
-                    Object.assign({}, properties || {}, this)
+                    Object.assign({}, properties, this)
                 ) as Gender;
             } else if (Math.floor(Math.random() * NON_BINARY_CHANCE) === 1) {
                 this.gender = "Non-Binary";
             }
         }
 
-        if (properties && properties.age) {
+        if (properties && typeof properties.age === "string") {
             this.age = properties.age;
         } else {
             this.age = this.place.lists.age.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             );
         }
 
-        if (properties && properties.race) {
+        if (properties && typeof properties.race === "string") {
             this.race = properties.race;
         } else {
             this.race = this.place.lists.race.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             );
         }
 
-        if (properties && properties.class) {
+        if (properties && typeof properties.class === "string") {
             this.class = properties.class;
         } else {
             this.class = this.place.lists.class.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             );
         }
 
-        if (properties && properties.nobility) {
+        if (properties && typeof properties.nobility === "string") {
             this.nobility = properties.nobility;
         } else {
             this.nobility = this.place.lists.nobility.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             );
         }
 
-        if (properties && properties.title) {
+        if (properties && typeof properties.title === "string") {
             this.title = properties.title;
         } else if (this.nobility === "Knighted") {
             this.title = this.gender === "Male" ? "Sir" : "Dame";
         } else {
             this.title = this.place.lists.title.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             );
         }
 
-        if (properties && properties.alignment) {
+        if (properties && typeof properties.alignment === "string") {
             this.alignment = properties.alignment;
         } else {
             this.alignment = this.place.lists.alignment.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             );
         }
 
-        if (properties && properties.motivation) {
+        if (properties && typeof properties.motivation === "string") {
             this.motivation = properties.motivation;
         } else {
             this.motivation = this.place.lists.motivation.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             );
         }
 
-        if (properties && properties.profession) {
+        if (properties && typeof properties.profession === "string") {
             this.profession = properties.profession;
         } else {
             this.profession = this.place.lists.profession.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             );
         }
 
         this.surname = "";
-        if (properties && properties.forename) {
+        if (properties && typeof properties.forename === "string") {
             this.forename = properties.forename;
 
             if (properties.surname) this.surname = properties.surname;
         } else {
             let [forename, surname] = new NameList().pickRandom().split(" ");
-
-            if (properties && properties.surname) surname = properties.surname;
+            if (properties && typeof properties.surname === "string") {
+                surname = properties.surname;
+            }
 
             this.forename = (forename || "").trim();
             this.surname = (surname || "").trim();
         }
 
-        if (properties && properties.hair) {
+        if (properties && typeof properties.hair === "string") {
             this.hair = properties.hair;
         } else {
             this.hair = this.place.lists.hair.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             );
         }
 
-        if (properties && properties.eye) {
-            this.eye = properties.eye;
+        if (properties && typeof properties.eyes === "string") {
+            this.eyes = properties.eyes;
         } else {
             do {
-                this.eye = this.place.lists.eye.pickRandom(
-                    Object.assign({}, properties || {}, this)
+                this.eyes = this.place.lists.eye.pickRandom(
+                    Object.assign({}, properties, this)
                 );
-            } while (this.eye === this.hair);
+            } while (this.eyes === this.hair);
         }
 
-        if (properties && properties.armor) {
+        if (properties && typeof properties.armor === "string") {
             this.armor = properties.armor;
         } else {
             this.armor = this.place.lists.armor.pickRandom(
-                Object.assign({}, properties || {}, this)
+                Object.assign({}, properties, this)
             );
         }
 
-        if (properties && properties.weapons) {
+        if (properties && typeof properties.weapons === "string") {
             this.weapons = properties.weapons;
         } else {
             this.weapons = new Array(Math.floor(Math.random() * 2) + 1)
                 .fill(null)
                 .map(() => {
                     return this.place.lists.weapon.pickRandom(
-                        Object.assign({}, properties || {}, this)
+                        Object.assign({}, properties, this)
                     );
                 });
 
@@ -191,16 +192,23 @@ export class NPC {
     }
 
     getDescription() {
+        let hair_eyes: string[] | string = [
+            this.hair ? `${this.hair} hair` : "",
+            this.eyes ? `${this.eyes} eyes` : ""
+        ].filter(value => value);
+        hair_eyes = hair_eyes.length ? `with ${hair_eyes.join(" and ")}` : "";
+
+        let pronoun = getPronoun("third", this.gender, "subject");
+        pronoun = hair_eyes ? `. ${ucfirst(pronoun)}` : `, ${pronoun}`;
+
         return `${this.getName()} (${this.profession}): ${ucfirst(this.age)} ${
             this.race
         } ${this.class}, ${this.alignment}. ${
             this.forename
-        } is {physical detail} with ${this.hair} hair and ${
-            this.eye
-        } eyes. ${ucfirst(getPronoun("third", this.gender, "subject"))} wears ${
+        } is {physical detail}${hair_eyes + pronoun} wears ${
             this.armor
-        } and wields a ${this.weapons.join(" and a ")}. ${
-            this.forename
-        } seeks ${this.motivation}.`;
+        } and wields a ${this.weapons.join(" and ")}. ${this.forename} seeks ${
+            this.motivation
+        }.`;
     }
 }
