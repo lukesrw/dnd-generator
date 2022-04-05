5f9d7bd04bff321594311f91782042be6a061c46
lukesrw <lukesrw+github@gmail.com>
2022-04-05 18:10:15 +0100

    Updated interface with new values

--- lib/list/race/race.ts
+++ lib/list/race/race.ts
@@ -1,13 +1,33 @@
-import { List } from "../../List";
-import { PickList } from "../../List";
+import { List, PickList } from "../../List";
+import { fNGSupportedRaces } from "../name/name";
+
+export interface RaceProperties {
+    /**
+     * Filters
+     */
+    class: string[];
+
+    /**
+     * Properties
+     */
+    speed: number;
+    languages: PickList;
+
+    // for use in NameList
+    names: (keyof typeof fNGSupportedRaces)[];
+
+    // for use in AgeList
+    age: {
+        [maturity: string]: [number, number];
+        infant: [number, number];
+        child: [number, number];
+        adult: [number, number];
+        elder: [number, number];
+    };
+}
 
 export class RaceList extends List<RaceProperties> {
     constructor() {
         super([]);
     }
-}
-
-export interface RaceProperties {
-    speed: number;
-    languages?: PickList;
 }
