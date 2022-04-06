712f7b481b4931e124edf1ae6095ced8bf435a8f
lukesrw <lukesrw+github@gmail.com>
2022-04-06 12:32:50 +0100

    Updated age test to check maturities

--- lib/list/age/age.test.ts
+++ lib/list/age/age.test.ts
@@ -1,15 +1,20 @@
+import { MaturityList } from "../maturity/maturity";
 import { RaceList } from "../race/race";
 import { AgeList } from "./age";
 
 let ages = new AgeList();
 let races = new RaceList();
+let maturities = new MaturityList();
 
-races.getValues().forEach((race) => {
-    test(`AgeList supports "${race}" race`, () => {
-        expect(() => {
-            ages.pickRandom({
-                race: race
-            });
-        }).not.toThrow();
+races.getValues().forEach(race => {
+    maturities.getValues().forEach(maturity => {
+        test(`AgeList supports "${race}" race and "${maturity}" maturity`, () => {
+            expect(() => {
+                ages.pickRandom({
+                    race: race,
+                    maturity: maturity
+                });
+            }).not.toThrow();
+        });
     });
 });
