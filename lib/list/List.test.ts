fc28d110fc85be0cc746da38970f648b2ccb849c
lukesrw <lukesrw+github@gmail.com>
2022-04-16 21:26:43 +0100

    Added test for multiple classes

--- lib/list/List.test.ts
+++ lib/list/List.test.ts
@@ -120,3 +120,53 @@ test("getFiltered 50/50 Test (classes)", () => {
     expect(pick2SeenItem1).toBeFalsy();
     expect(pick2SeenItem2).toBeTruthy();
 });
+
+test("getFiltered 50/50 Test (multiple classes)", () => {
+    let runFor = 10;
+
+    let item1 = { classes: ["Druid", "Fighter"], value: "One" };
+    let item2 = { value: "Two" };
+
+    let list = new List<{ classes: string[] }>([item1, item2]);
+
+    let pick1SeenItem1 = false;
+    let pick1SeenItem2 = false;
+    let pick2SeenItem1 = false;
+    let pick2SeenItem2 = false;
+
+    let shouldGetBoth = {
+        classes: [{ name: item1.classes[0], level: 1 }]
+    };
+
+    let shouldGetOne = {
+        classes: [
+            { name: "Druid", level: 1 },
+            { name: "Bard", level: 1 }
+        ]
+    };
+
+    let i = 0;
+    for (i; i < runFor; i += 1) {
+        let pick1 = list.pickRandom(shouldGetBoth);
+
+        if (pick1 === item1.value) pick1SeenItem1 = true;
+        if (pick1 === item2.value) pick1SeenItem2 = true;
+
+        if (pick1SeenItem1 && pick1SeenItem2) break;
+    }
+
+    expect(pick1SeenItem1).toBeTruthy();
+    expect(pick1SeenItem2).toBeTruthy();
+
+    for (i = 0; i < runFor; i += 1) {
+        let pick2 = list.pickRandom(shouldGetOne);
+
+        if (pick2 === item1.value) pick2SeenItem1 = true;
+        if (pick2 === item2.value) pick2SeenItem2 = true;
+
+        if (pick2SeenItem1 && pick2SeenItem2) break;
+    }
+
+    expect(pick2SeenItem1).toBeFalsy();
+    expect(pick2SeenItem2).toBeTruthy();
+});
