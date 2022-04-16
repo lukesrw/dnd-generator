64f63caa6eb6ecf3c802d3dc05d864ac99793a3e
lukesrw <lukesrw+github@gmail.com>
2022-04-16 16:25:07 +0100

    Added tests for two kinds of filtering

--- lib/list/List.test.ts
+++ lib/list/List.test.ts
@@ -26,3 +26,97 @@ test("Pick List doesn't duplicate (callback)", () => {
 
     expect(new Set(items).size).toEqual(randomItems);
 });
+
+test("getFiltered 50/50 Test (any)", () => {
+    let runFor = 10;
+
+    let item1 = { maturity: "adult", value: "One" };
+    let item2 = { value: "Two" };
+
+    let list = new List<{ classes: string[] }>([item1, item2]);
+
+    let pick1SeenItem1 = false;
+    let pick1SeenItem2 = false;
+    let pick2SeenItem1 = false;
+    let pick2SeenItem2 = false;
+
+    let shouldGetBoth = {
+        maturity: "adult"
+    };
+
+    let shouldGetOne = {
+        maturity: "elder"
+    };
+
+    let i = 0;
+    for (i; i < runFor; i += 1) {
+        let pick1 = list.pickRandom(shouldGetBoth);
+
+        if (pick1 === item1.value) pick1SeenItem1 = true;
+        if (pick1 === item2.value) pick1SeenItem2 = true;
+
+        if (pick1SeenItem1 && pick1SeenItem2) break;
+    }
+
+    expect(pick1SeenItem1).toBeTruthy();
+    expect(pick1SeenItem2).toBeTruthy();
+
+    for (i = 0; i < runFor; i += 1) {
+        let pick2 = list.pickRandom(shouldGetOne);
+
+        if (pick2 === item1.value) pick2SeenItem1 = true;
+        if (pick2 === item2.value) pick2SeenItem2 = true;
+
+        if (pick2SeenItem1 && pick2SeenItem2) break;
+    }
+
+    expect(pick2SeenItem1).toBeFalsy();
+    expect(pick2SeenItem2).toBeTruthy();
+});
+
+test("getFiltered 50/50 Test (classes)", () => {
+    let runFor = 10;
+
+    let item1 = { classes: ["Druid"], value: "One" };
+    let item2 = { value: "Two" };
+
+    let list = new List<{ classes: string[] }>([item1, item2]);
+
+    let pick1SeenItem1 = false;
+    let pick1SeenItem2 = false;
+    let pick2SeenItem1 = false;
+    let pick2SeenItem2 = false;
+
+    let shouldGetBoth = {
+        classes: [{ name: item1.classes[0], level: 1 }]
+    };
+
+    let shouldGetOne = {
+        classes: [{ name: "Fighter", level: 1 }]
+    };
+
+    let i = 0;
+    for (i; i < runFor; i += 1) {
+        let pick1 = list.pickRandom(shouldGetBoth);
+
+        if (pick1 === item1.value) pick1SeenItem1 = true;
+        if (pick1 === item2.value) pick1SeenItem2 = true;
+
+        if (pick1SeenItem1 && pick1SeenItem2) break;
+    }
+
+    expect(pick1SeenItem1).toBeTruthy();
+    expect(pick1SeenItem2).toBeTruthy();
+
+    for (i = 0; i < runFor; i += 1) {
+        let pick2 = list.pickRandom(shouldGetOne);
+
+        if (pick2 === item1.value) pick2SeenItem1 = true;
+        if (pick2 === item2.value) pick2SeenItem2 = true;
+
+        if (pick2SeenItem1 && pick2SeenItem2) break;
+    }
+
+    expect(pick2SeenItem1).toBeFalsy();
+    expect(pick2SeenItem2).toBeTruthy();
+});
