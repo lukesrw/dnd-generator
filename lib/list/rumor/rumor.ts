b941de92444d033489e48e41faef9cdcfc5cb7c3
lukesrw <lukesrw+github@gmail.com>
2022-04-20 22:58:05 +0100

    Initial prototyping of RumorList

---
+++ lib/list/rumor/rumor.ts
@@ -0,0 +1,88 @@
+import {
+    Generator as SourceGenerator,
+    List as SourceList
+} from "../../../index";
+import * as Generic from "../../../interfaces/generic";
+import { NPC } from "../../generator/NPC";
+import { List } from "../List";
+
+export const regexRumorVariable =
+    /\{(?<source>\w+)(?:\[(?<name>\w+)\])?:(?<property>\w+)?\}/giu;
+
+export class RumorList extends List {
+    constructor() {
+        super([]);
+    }
+
+    static parse(
+        rumor: string,
+        initialMemory: Generic.Object<Generic.Object<any>> = {}
+    ) {
+        let memory: Generic.Object<Generic.Object<any>> = initialMemory;
+
+        return rumor.replaceAll(
+            regexRumorVariable,
+            (match, source, name, property) => {
+                /**
+                 * ensure source exists in memory
+                 */
+                if (!(source in memory)) {
+                    memory[source] = {};
+                }
+
+                /**
+                 * automatically assign name if undefined
+                 */
+                if (typeof name === "undefined") {
+                    name = (
+                        Object.values(memory[source]).length + 1
+                    ).toString();
+                }
+
+                /**
+                 * retrieve from memory/generate if not found
+                 */
+                let item = memory[source][name];
+                if (typeof item === "undefined") {
+                    if (source in SourceGenerator) {
+                        item = new SourceGenerator[
+                            source as keyof typeof SourceGenerator
+                        ]({} as any);
+                    } else if (source in SourceList.Professions) {
+                        item = new SourceList.Professions[
+                            source as keyof typeof SourceList.Professions
+                        ]();
+                    } else if (source in SourceList) {
+                        item = new (SourceList[
+                            source as keyof typeof SourceList
+                        ] as any)();
+                    } else {
+                        throw new Error(`Unable to find source: ${source}`);
+                    }
+
+                    memory[source][name] = item;
+                }
+
+                /**
+                 * replace variable with value
+                 */
+                if (typeof item === "object") {
+                    switch (typeof item[property]) {
+                        case "string":
+                        case "number":
+                            return item[property];
+
+                        case "function":
+                            return item[property]();
+                    }
+                }
+
+                throw new Error(`Unable to replace ${match}`);
+            }
+        );
+    }
+
+    pickRandom(filter: Partial<NPC> = {}) {
+        return RumorList.parse(super.pickRandom(filter));
+    }
+}
