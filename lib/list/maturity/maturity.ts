9700da9df051fcc6f1df51c9ab3d784e6d6a0b22
lukesrw <lukesrw+github@gmail.com>
2022-04-26 18:50:38 +0100

    Removed maturities.json, added lists for age ranges

--- lib/list/maturity/maturity.ts
+++ lib/list/maturity/maturity.ts
@@ -1,7 +1,43 @@
 import { List } from "../List";
 
+let maturities = [
+    {
+        value: "infant"
+    },
+    {
+        value: "child",
+        weight: 3
+    },
+    {
+        value: "adult",
+        weight: 12
+    },
+    {
+        value: "elder",
+        weight: 4
+    }
+];
+
 export class MaturityList extends List {
     constructor() {
-        super([]);
+        super(maturities);
+    }
+}
+
+export class ChildMaturityList extends List {
+    constructor() {
+        super(maturities.slice(1));
+    }
+}
+
+export class AdultMaturityList extends List {
+    constructor() {
+        super(maturities.slice(2));
+    }
+}
+
+export class ElderMaturityList extends List {
+    constructor() {
+        super(maturities.slice(3));
     }
 }
